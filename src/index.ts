4e3222301c1ad9a8cc44d66921f5f69e8dee9f5b
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-08-02 19:42:32 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,8 @@ app.use(
 );
 
 app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   console.log(`${req.method} ${req.url}`);
   next();
 });
