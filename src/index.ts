290270e5affddc7ff8f115ce0474dde833b1e048
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-08-09 02:52:27 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -92,6 +92,7 @@ connectDB();
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
+  console.log("---------------BACKEND -----------")
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
